723dcc0a6ae647190d708405fc43776609f7e840
David Selck <dselck@pop-os.localdomain>
2020-12-30 21:44:37 +0930

    add a run_once script

---
+++ scripts/run_xmltv_pulls_once.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+import os
+import sys
+import time
+import epg_tool
+import tmdbsimple as tmdb
+
+# If the environment variables aren't set let's cancel
+if not os.getenv('DATA_VOLUME') or \
+        not os.getenv('MOVIEDB_KEY') or \
+        not os.getenv('XMLTV_URL') or \
+        not os.getenv('TVHEADEND_URL'):
+    print('Not all environment variables set properly')
+    sys.exit(2)
+
+# Collect the Variables
+data_vol = os.getenv('DATA_VOLUME')
+apikey = os.getenv('MOVIEDB_KEY')
+cachedir = os.path.join(data_vol, 'tv_cache')
+internet_url = os.getenv('XMLTV_URL')
+xmltv_save = os.path.join(data_vol, 'xmltv.xml')
+tvheadend_url = os.getenv('TVHEADEND_URL')
+
+# Do some setup
+tmdb.API_KEY = apikey
+enricher = epg_tool.enricher(cachedir)
+
+# Pull the files that we are going to need
+internet_programs, internet_channels, internet_df = epg_tool.parse_xml(internet_url)
+tvhd_programs, tvhd_channels, tvhd_df = epg_tool.parse_xml(tvheadend_url)
+
+# Fix the channels for tvhd to match internet
+tvhd_channels = epg_tool.transfer_channel_ids(tvhd_channels, tvhd_programs, internet_channels)
+
+# Pull the data from the internet programs (bad times) to the local times
+tvhd_programs, tvhd_channels = epg_tool.match_headend_to_internet(tvhd_programs,
+                                                                tvhd_channels,
+                                                                internet_programs,
+                                                                internet_channels,
+                                                                internet_df)
+
+# Now we can enrich all of the data!
+for i in range(len(tvhd_programs)):
+    if tvhd_df.iloc[i].Is_Movie:
+        tvhd_programs[i] = enricher.update_movie_program(tvhd_programs[i])
+    else:
+        tvhd_programs[i] = enricher.update_series_program(tvhd_programs[i])
+
+# We can now save all this to disk
+epg_tool.write_xml(tvhd_programs, tvhd_channels, xmltv_save)
